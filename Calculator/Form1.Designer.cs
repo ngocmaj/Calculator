1cd25ac9764f5d03ed9ba78581ef60e03abe7466
ngocmai <mainguyentet@gmail.com>
2023-10-27 09:30:39 +0700

    Xu ly cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -99,6 +99,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Cộng";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
